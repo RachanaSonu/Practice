73091df3b8615865a856bf44b5641485b0c82621
RachanaSonu <rachana.p1398@gmail.com>
2023-08-28 12:41:12 +0530

    n

--- Full Stack/Front End/Lib/web/src/Routers/index.ts
+++
@@ -1,7 +0,0 @@
-import { useRoutes } from "react-router-dom"
-import { Projectapp} from "./Project.router"
-
-export let Allroutes=()=>{
-
-    return useRoutes([Projectapp])
-}
--- Full Stack/Front End/Lib/web/src/service/getAllcorona.service.ts
+++
@@ -1,7 +0,0 @@
-export let getAllcorona=async()=>{
-
-    let data=await fetch('https://data.covid19india.org/data.json')
-    data=await(await data).json()
-    console.log("data",data)
-    return data
-}
